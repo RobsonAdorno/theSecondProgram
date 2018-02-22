5361d7ee58d5e896cc70acb31e8fd65a5cba065f
Robson Adorno <rferreira@scopus.com.br>
2018-02-22 17:23:41 -0300

    Alteracoes no codigo do conversor

--- Conversores/ViewController.swift
+++ Conversores/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var textFiledValue: UITextField!
     @IBOutlet weak var lblResult: UILabel!
     @IBOutlet weak var lblResultUnit: UILabel!
+    @IBOutlet weak var lbUnity: UILabel!
     
     
     override func viewDidLoad() {
@@ -28,9 +29,60 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showNext(_ sender: UIButton) {
+        switch lbUnity.text!{
+        case "Temperatura":
+            lbUnity.text = "Peso"
+            btnUnit1.setTitle("Kilograma", for: UIControlState.normal)
+            btnUnity2.setTitle("Libra", for: UIControlState.normal)
+        case "Peso":
+            lbUnity.text = "Moeda"
+            btnUnit1.setTitle("Real", for: UIControlState.normal)
+            btnUnity2.setTitle("Dólar", for: UIControlState.normal)
+        case "Moeda":
+            lbUnity.text = "Distância"
+            btnUnit1.setTitle("Metro", for: UIControlState.normal)
+            btnUnity2.setTitle("Kilômetro", for: UIControlState.normal)
+        default:
+            lbUnity.text = "Temperatura"
+            btnUnit1.setTitle("Celsius", for: UIControlState.normal)
+            btnUnity2.setTitle("Farenheint", for: UIControlState.normal)
+        }
+        convert(nil)
     }
     
-    @IBAction func convert(_ sender: UIButton) {
+    @IBAction func convert(_ sender: UIButton?) {
+        if let sender = sender{
+            if sender == btnUnit1{
+                btnUnity2.alpha = 0.5
+            }else{
+                btnUnit1.alpha = 0.5
+            }
+            sender.alpha = 1.0
+        }
+        switch lbUnity.text!{
+        case "Temperatura":
+          calcTemperature()
+        case "Peso":
+           calcWeight()
+        case "Moeda":
+           calcCurrency()
+        default:
+            calcDistance()
+            
+        }
+    }
+    func calcTemperature(){
+        
+    }
+    func calcWeight(){
+        
+    }
+    func calcCurrency(){
+        
+    }
+    
+    func calcDistance(){
+        
     }
 }
 
