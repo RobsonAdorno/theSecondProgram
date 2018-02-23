08840e61a6cd9ebcf1bc7c69cb87f5da25de774a
Robson Adorno <rferreira@scopus.com.br>
2018-02-23 09:03:27 -0300

    Modificando teclado e calculo na aplicacao

--- Conversores/ViewController.swift
+++ Conversores/ViewController.swift
@@ -70,19 +70,52 @@ class ViewController: UIViewController {
             calcDistance()
             
         }
+        view.endEditing(true)
+        let result = Double(lblResult.text!)!
+        lblResult.text = String(format: "%.2f",result)
     }
     func calcTemperature(){
-        
+        //Guard tem uma visualizacao global, mesmo voce fazendo um guard let na hora de desenbrulhar uma variavel a mesma fica com uma visualizacao global
+        guard let temperature = Double(textFiledValue.text!)else{return}
+        if btnUnit1.alpha == 1.0{
+            lblResult.text = "Farenheint"
+            lblResult.text = String(temperature * 1.8 + 32.0)
+        }else{
+            lblResult.text = "Celsius"
+            lblResult.text = String((temperature - 32.0) / 1.8)
+        }
     }
     func calcWeight(){
-        
+        guard let weight = Double(textFiledValue.text!)else{return}
+        if btnUnit1.alpha == 1.0{
+            lblResult.text = "Libra"
+            lblResult.text = String(weight / 2.2046)
+        }else{
+            lblResult.text = "KIlograma"
+            lblResult.text = String(weight * 2.2046)
+        }
     }
     func calcCurrency(){
-        
+        guard let currency = Double(textFiledValue.text!)else{return}
+        if btnUnit1.alpha == 1.0{
+            lblResult.text = "Dólar"
+            lblResult.text = String(currency / 3.5)
+        }else{
+            lblResult.text = "Real"
+            lblResult.text = String(currency * 3.5)
+            
+        }
     }
     
     func calcDistance(){
-        
+        guard let distance = Double(textFiledValue.text!)else{return}
+        if btnUnit1.alpha == 1.0{
+            lblResult.text = "Kilometro"
+            lblResult.text = String(distance / 1000.0)
+        }else{
+            lblResult.text = "Metros"
+            lblResult.text = String(distance * 1000.0)
+        }
     }
 }
 
